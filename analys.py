b0ee12e3ae86844e627017359a7f46c73a62d67b
RUGANT9 <50453369+RUGANT9@users.noreply.github.com>
2020-07-22 15:10:19 +0530

    Add files via upload

---
+++ analys.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 11:29:57 2020
+
+@author: ADMIN
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('Placement_Data_Full_Class.csv')
+print(data.head())
+data = data.dropna()
